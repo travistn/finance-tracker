67c63120a9c6188d097252a3161c8ab41219cb4f
travistn <travisn714@gmail.com>
2025-03-04 19:56:10 -0800

    Create a reusable Button component

---
+++ components/Button.tsx
@@ -0,0 +1,34 @@
+interface ButtonProps {
+  children: React.ReactNode;
+  type?: 'button' | 'submit' | 'reset';
+  variant?: 'primary' | 'secondary' | 'destroy';
+  className?: string;
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+const Button = ({
+  children,
+  type = 'button',
+  variant = 'primary',
+  className = '',
+  onClick,
+}: ButtonProps) => {
+  const variants = {
+    primary: 'bg-gray-900 text-white hover:bg-gray-500',
+    secondary: 'bg-beige-100 text-gray-900 hover:bg-white hover:border hover:border-beige-500',
+    destroy: 'bg-red text-white hover:opacity-80',
+  };
+
+  return (
+    <button
+      type={type}
+      disabled={false}
+      onClick={onClick}
+      className={`flex-row-center p-4 rounded-lg cursor-pointer text-preset-4-bold ${variants[variant]} ${className}`}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
