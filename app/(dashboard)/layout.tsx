d641c2282e62091352b2c7dab73b6b37b7bcbb6f
travistn <travisn714@gmail.com>
2025-03-19 12:54:13 -0700

    Edit the layout so that the Sidebar height on large screens will fully extend down

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,10 +3,12 @@ import Sidebar from '@/components/Sidebar';
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className='xl:flex'>
-      <div className='max-xl:fixed max-xl:bottom-0 max-xl:w-full flex flex-col xl:flex-row xl:h-screen'>
+      <div className='max-xl:fixed max-xl:bottom-0 max-xl:w-full flex flex-col xl:flex-row xl:min-h-screen'>
         <Sidebar />
       </div>
-      <main className='flex flex-col px-4 py-6 md:px-10 md:py-8 w-full'>{children}</main>
+      <main className='flex flex-col px-4 py-6 md:px-10 md:py-8 w-full max-xl:mb-20'>
+        {children}
+      </main>
     </div>
   );
 }
