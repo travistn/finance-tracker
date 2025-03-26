71b909962ba63a2377f02b7b89b9bee648192503
travistn <travisn714@gmail.com>
2025-03-26 14:32:56 -0700

    Define POST function to create new budgets

--- app/api/budget/route.ts
+++ app/api/budget/route.ts
@@ -3,14 +3,26 @@ import { NextResponse } from 'next/server';
 import connectToDatabase from '@/lib/mongoose';
 import Budget from '@/models/Budget';
 
+export const GET = async () => {
+  try {
+    await connectToDatabase();
+
+    const budgets = await Budget.find();
+    return NextResponse.json(budgets, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Failed to get budgets' }, { status: 400 });
+  }
+};
+
 export const POST = async (req: Request) => {
   try {
     await connectToDatabase();
 
     const body = await req.json();
 
-    const { budgetMaximum, category, theme } = body.budgetFormData;
-    const newBudget = new Budget({ category, theme, maximum: budgetMaximum });
+    const { category, maximum, theme } = body.budget;
+    const newBudget = new Budget({ category, maximum, theme });
 
     await newBudget.save();
     return NextResponse.json(newBudget, { status: 201 });
