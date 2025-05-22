c429141df8b9c8ffe99c776b2c0f7b050170d117
travistn <travisn714@gmail.com>
2025-05-22 16:31:04 -0700

    Install and implement date-fns to display the date of a recurring bill

--- components/RecurringTransaction.tsx
+++ components/RecurringTransaction.tsx
@@ -1,3 +1,5 @@
+import { getDate, format } from 'date-fns';
+
 import { TransactionType } from '@/types';
 
 interface RecurringTransactionProps {
@@ -17,21 +19,28 @@ const RecurringTransaction = ({ transaction }: RecurringTransactionProps) => {
       </span>
       <div className='flex items-center justify-between md:col-start-5'>
         <span className='flex items-center gap-2'>
-          <p className='text-preset-5 text-green'>Monthly-1st</p>
+          <p className='text-preset-5 text-green'>{`Monthly - ${format(
+            getDate(transaction.date),
+            'do'
+          )}`}</p>
           <img src='/assets/images/icon-bill-paid.svg' />
         </span>
         <p className='text-preset-4-bold text-gray-900 md:hidden'>
-          {transaction.amount.toLocaleString('en-US', {
-            style: 'currency',
-            currency: 'USD',
-          })}
+          {transaction.amount
+            .toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            })
+            .slice(1)}
         </p>
       </div>
       <p className='text-preset-4-bold text-gray-900 max-md:hidden text-right'>
-        {transaction.amount.toLocaleString('en-US', {
-          style: 'currency',
-          currency: 'USD',
-        })}
+        {transaction.amount
+          .toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD',
+          })
+          .slice(1)}
       </p>
     </div>
   );
