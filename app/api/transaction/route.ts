9c2c176c346ab331c746432e0f10ac7b044450a3
Travis Nguyen <tnguy190@wgu.edu>
2025-10-14 16:48:33 -0700

    Edit GET function to retrieve transactions that only match to userID

--- app/api/transaction/route.ts
+++ app/api/transaction/route.ts
@@ -1,13 +1,16 @@
 import { NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
 
 import connectToDatabase from '@/lib/mongoose';
 import Transaction from '@/models/Transaction';
 
-export const GET = async (req: any, res: any) => {
+export const GET = async () => {
   try {
     await connectToDatabase();
 
-    const transactions = await Transaction.find();
+    const session = await getServerSession(authOptions);
+    const transactions = await Transaction.find({ userId: session?.user.id });
 
     return NextResponse.json(transactions, { status: 200 });
   } catch (error) {
